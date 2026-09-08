0f2a6c1f0d61db349b9fbbc644687507e9156df1
Marcella <marcellapinheirosilva@gmail.com>
2026-09-08 17:13:16 -0300

    feat: adiciona estados ao formulário de tarefas

--- src/components/FormularioTarefa.jsx
+++ src/components/FormularioTarefa.jsx
@@ -1,7 +1,59 @@
+import { useState } from "react";
 function FormularioTarefa() {
+  const [nome, setNome] = useState("");
+  const [data, setData] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const [prioridade, setPrioridade] = useState("");
+
   return (
     <section>
       <h2>Nova Tarefa</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="nome">Nome da tarefa</label>
+          <input
+          id = "nome"
+          type = "text"
+          value = {nome}
+          onChange = {(evento) => setNome(evento.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="data">Data</label>
+          <input
+          id = "data"
+          type = "date"
+          value = {data}
+          onChange = {(evento) => setData(evento.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="descricao">Descrição</label>
+          <textarea
+          id = "descricao"
+          value = {descricao}
+          onChange = {(evento) => setDescricao(evento.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="prioridade">Nível de prioridade</label>
+          <select
+          id = "prioridade"
+          value = {prioridade}
+          onChange = {(evento) => setPrioridade(evento.target.value)}
+          >
+            <option value="">Selecione uma prioridade</option>
+            <option value="Baixa">Baixa</option>
+            <option value="Média">Média</option>
+            <option value="Alta">Alta</option>
+          </select>
+        </div>
+        <button type="submit">Adicionar Tarefa</button>
+      </form>
     </section>
   );
 }
