76d36557cfd4f78b6ea716b119a6606ccfa266c7
Isabella <isabellayk13@gmail.com>
2026-09-08 19:53:44 -0300

    feat: implementa listagem de tarefas

--- src/components/ListaTarefas.jsx
+++ src/components/ListaTarefas.jsx
@@ -1,8 +1,49 @@
-function ListaTarefas() {
+
+import Tarefa from "./Tarefa";
+
+
+function ListaTarefas( {
+  tarefas,
+  filtro,
+  onConcluirTarefa,
+  onRemoverTarefa,
+}) {
+  // filter() cria uma nova lista contendo apenas as tarefas
+  // que correspondem ao filtro selecionado pelo usuário.
+  const tarefasFiltradas = tarefas.filter((tarefa) => {
+    if (filtro === "pendentes") {
+      return !tarefa.concluida;
+    }
+
+    if (filtro === "concluidas") {
+      return tarefa.concluida;
+    }
+
+    return true;
+  });
+
+
   return (
-    <section>
+    <>
+     <section>
       <h2>Minhas Tarefas</h2>
+
+      {tarefasFiltradas.length === 0 ? (
+        <p>Nenhuma tarefa encontrada.</p>
+      ) : (
+        // map() percorre a lista filtrada e cria um componente
+        // Tarefa para cada tarefa existente.
+        tarefasFiltradas.map((tarefa) => (
+          <Tarefa
+            key={tarefa.id}
+            tarefa={tarefa}
+            onConcluirTarefa={onConcluirTarefa}
+            onRemoverTarefa={onRemoverTarefa}
+          />
+        ))
+      )}
     </section>
+    </>
   );
 }
 
